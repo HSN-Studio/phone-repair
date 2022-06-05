b011ae677b78c42b6f79a7c3bb8a80cc2736ebd0
HSN-Studio <hassanrafique198@gmail.com>
2022-06-05 12:26:28 +0500

    Context Api Used first time!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import "./App.css";
-
+import SelectDevice from "./components/SelectDevice";
+import StepProvider from "./Store/StepProvider";
 function App() {
+  // States
+
+  // Regular Methods
+  // JSX
   return (
     <div className="App">
-      <header className="App-header">
-        Started working on Wefixtech Repair device App!
-      </header>
+      <header className="App-header"></header>
+      <StepProvider>
+        <SelectDevice />
+      </StepProvider>
     </div>
   );
 }
