74563bc2158b7b4e6b0a9c86123701c43acf5d91
HSN-Studio <hassanrafique198@gmail.com>
2022-06-08 10:54:34 +0500

    Added Devices Models Fuctionality!

--- src/components/SelectModel.js
+++ src/components/SelectModel.js
@@ -12,6 +12,8 @@ function SelectModel() {
   //States
   const [devices, setdevices] = useState();
   const [brands, setbrands] = useState([]);
+  const [models, setmodels] = useState([]);
+  const [brand, setbrand] = useState(undefined);
   useEffect(() => {
     const Devices = fetch(`./${deviceType}.json`)
       .then((res) => res.json())
@@ -20,6 +22,13 @@ function SelectModel() {
         setbrands(Object.keys(data));
       });
   }, []);
+  useEffect(() => {
+    if (brand === undefined) return;
+    const modelsData = devices[brand].map((device) => {
+      return device.Model;
+    });
+    setmodels(modelsData);
+  }, [brand]);
 
   //Regular Functions
 
@@ -33,6 +42,14 @@ function SelectModel() {
         options={brands}
         sx={{ width: 300 }}
         renderInput={(params) => <TextField {...params} label="Brand" />}
+        onChange={(e, value) => setbrand(value)}
+      />
+      <Autocomplete
+        disablePortal
+        id="select-device-model"
+        options={models}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Model" />}
       />
     </div>
   );
